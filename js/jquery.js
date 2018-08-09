3fdeb5f8106fc2985eec1cae894e6239c0131bef
martintirtawisata <martin.tirtawisata@hotmail.com>
2018-08-09 13:10:46 -0700

    changes

--- js/jquery.js
+++ js/jquery.js
@@ -2,3 +2,7 @@ function renderUserInfo(routeInfo, index){
     $('.js-map-leg-info').append(`<div class="route-info"><p><span class="start-left"><strong>Start:</strong> ${routeInfo[index].start_name}</span><span class="start-right"><strong>End: </strong> ${routeInfo[index].end_name}</span>
     </p><p><strong>Duration:</strong> <span class="info-duration">${routeInfo[index].routeDuration}</span> <strong>Distance: </strong><span class="info-distance"> ${routeInfo[index].routeDistance}</span>   <span class="icon"><i class="fas fa-car"></i></span>`) 
 }
+
+$('.input-container').on('click','#place-input', function(){
+    $('.desc').css('display','block')
+});
